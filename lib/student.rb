d9fced86c2b77cffcba61039d6307c2a5ddf5088
matomugo60 <matomugo60@gmail.com>
2023-02-13 15:30:48 +0300

    Complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+      super
+      @knowledge = []
+    end
+  
+    def learn(knowledge_string)
+      @knowledge << knowledge_string
+    end
+  
 
 end
